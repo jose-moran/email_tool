a9957fee45eff712f4e383988742eae455140eca
José Moran <jose.moran@polytechnique.org>
2024-08-08 12:25:05 +0200

    fix filename bug

--- email_tool/email_msg.py
+++ email_tool/email_msg.py
@@ -1,12 +1,11 @@
+import os
 from datetime import datetime
 
 import pytz
 
 
 def email_msg(msg: str) -> str:
-    import inspect
-
-    caller_filename = inspect.stack()[1].filename
+    caller_filename = get_importer()
 
     formatted_message = f"""Script in file: {caller_filename}
     
@@ -16,6 +15,33 @@ def email_msg(msg: str) -> str:
     """
 
     return formatted_message
+
+
+def get_importer():
+    import inspect
+
+    # Function to determine if a frame is part of the email_tool package
+    def is_internal_caller(_frame):
+        module = inspect.getmodule(_frame)
+        module_name = module.__name__ if module else ""
+        return "email_tool" in module_name
+
+    frame = inspect.currentframe()
+    caller_frame = frame
+
+    # Navigate back through the stack to find the first frame not in email_tool
+    while caller_frame and is_internal_caller(caller_frame):
+        caller_frame = caller_frame.f_back
+
+    # Get the filename from the caller's frame, or default to 'Unknown' if not found
+    if caller_frame:
+        caller_filename = caller_frame.f_globals.get("__file__", "Unknown")
+        caller_filename = os.path.basename(
+            caller_filename
+        )  # Just the filename, not the full path
+    else:
+        caller_filename = "Unknown"
+    return caller_filename
 
 
 def get_time(timezone: str = "Europe/Paris") -> str:
